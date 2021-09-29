3f89c6896aa486d9f10bf8c15628e6fac99b8223
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-29 16:50:01 +0300

    Warning component done

--- react-d3/src/App.js
+++ react-d3/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import React from "react-bootstrap";
+
 import Books from "../src/data/fantasy.json";
 import { Container } from "react-bootstrap";
+import Warning from './component/Warning'
 
 function App() {
   return (
     <Container>
-      <div></div>
+       <Warning AlertMessage="Hello"/>
+      <div>
+      </div>
     </Container>
   );
 }
---
+++ react-d3/src/component/Warning.jsx
@@ -0,0 +1,12 @@
+import {Alert} from "react-bootstrap";
+
+const WarningComponent = ({AlertMessage}) => {
+  return(
+    <Alert  variant="dark">
+      This is a  alert—check it out!
+    </Alert>
+
+  )
+}
+
+export default WarningComponent
